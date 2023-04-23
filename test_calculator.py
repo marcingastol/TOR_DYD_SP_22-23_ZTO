3086fb2fc32b995eb6ac464255e592d14bc7bfb2
marcingastol <62604769+marcingastol@users.noreply.github.com>
2023-04-23 14:41:59 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,13 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-  assert add(10,20) == 35
+  assert add(10,20) == 30
   
 def test_subtract():
   assert subtract(10,20) == -10
+
+def test_multiply():
+  assert multiply(10,20) == 200
+  
+def test_divide():
+  assert divide(10,20) == 0,5
